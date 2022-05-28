4ba9d343e73157117ccf1472e76ab1b800bb2502
Arthur <arthurcoutinho1998@gmail.com>
2022-05-28 16:21:05 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/controlefrotas/ControleFrotasApplication.kt
@@ -0,0 +1,11 @@
+package br.com.controlefrotas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControleFrotasApplication
+
+fun main(args: Array<String>) {
+    runApplication<ControleFrotasApplication>(*args)
+}
